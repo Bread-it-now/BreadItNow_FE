dd04dd6641af56f0d9019c89a38c296709fa79cf
yjh1008 <exesea12@gmail.com>
2025-02-05 21:08:45 +0900

    design: tailwind 설정 동작 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <div>
-      <h1>메인페이지</h1>
+      <h1 className="text-primary">메인페이지</h1>
       <br />
       <Link href="/search">검색페이지 이동</Link>
     </div>

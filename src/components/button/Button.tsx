c8bb2e55cdf5ae021fae968b36e84af2988b31c4
Sunghyun627 <kksscc4@gmail.com>
2025-02-10 20:35:16 +0900

    feat:  컴포넌트 구현 (#6)

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,18 +1,47 @@
 "use client";
-import { MouseEventHandler } from "react";
+
+import { cn } from "@/utils/cn";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  scheme?: "default" | "primary";
+  variant?: "default" | "primary";
   children: React.ReactNode;
   disabled?: boolean;
   className?: string;
-  onClick: MouseEventHandler<HTMLButtonElement>;
   type?: "button" | "submit" | "reset";
+  fullWidth?: boolean;
 }
 
-const Button = () => {
-  return <button>{}</button>;
+const Button = ({
+  children,
+  disabled = false,
+  variant = "default",
+  type = "button",
+  fullWidth = false,
+  className,
+}: ButtonProps) => {
+  const baseStyles = cn(
+    `flex center justify-center items-center w-[120px] h-[52px] rounded-lg px-[24px] py-[14px] gap-[10px] ${fullWidth && "w-full"}`,
+  );
+
+  const variants = {
+    default: cn("bg-white", "border border-gray-200"),
+    primary: cn("bg-primary"),
+  };
+
+  const contentStyles = cn(
+    `flex items-center justify-center w-full h-full ${variant === "default" ? "text-black" : "text-white"}`,
+  );
+
+  return (
+    <button
+      className={cn(baseStyles, variants[variant], className)}
+      disabled={disabled}
+      type={type ?? "button"}
+    >
+      <div className={contentStyles}>{children}</div>
+    </button>
+  );
 };
 
 export default Button;

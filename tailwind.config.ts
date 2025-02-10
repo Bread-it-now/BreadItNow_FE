7e19a520a4e3287465da2ecec0bb3d06fc50dee0
Sunghyun627 <kksscc4@gmail.com>
2025-02-10 20:05:21 +0900

    feat: pretendard font 설정 (#6)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        gray900: "#1c1e20",
-        gray700: "#4e5052",
-        gray500: "#808284",
-        gray400: "#999b9d",
-        gray300: "#b2b4b6",
-        gray200: "#d7d9db",
-        gray100: "#e8eaec",
-        gray50: "#f2f4f6",
-        primary: "#ff7651",
-        secondary: "#26cf9f",
-        white: "#ffffff",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        gray900: '#1c1e20',
+        gray700: '#4e5052',
+        gray500: '#808284',
+        gray400: '#999b9d',
+        gray300: '#b2b4b6',
+        gray200: '#d7d9db',
+        gray100: '#e8eaec',
+        gray50: '#f2f4f6',
+        primary: '#ff7651',
+        secondary: '#26cf9f',
+        white: '#ffffff',
+      },
+
+      fontFamily: {
+        Pretendard: ['Pretendard', 'sans-serif'],
       },
     },
   },

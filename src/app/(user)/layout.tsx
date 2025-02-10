2bfba2f9da0604aa4a552ed95c8de94f57b4927a
Sunghyun627 <kksscc4@gmail.com>
2025-02-10 18:18:29 +0900

    refactor: user와 admin 페이지 분리 (#6)

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,13 @@
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <div className="w-full h-full max-w-[375px] max-h-[812px] bg-white border-1 mx-auto">
+        {children}
+      </div>
+    </div>
+  );
+}

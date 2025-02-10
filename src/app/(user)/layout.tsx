bcbd6073feb58f46f27b9a6cd40a000abfa76137
Sunghyun627 <kksscc4@gmail.com>
2025-02-10 20:43:18 +0900

    feat: 모바일에 맞춰 레이아웃 수정 (#6)

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -4,8 +4,8 @@ export default function UserLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      <div className="w-full h-full max-w-[375px] max-h-[812px] bg-white border-1 mx-auto">
+    <div className="min-h-screen flex justify-center bg-gray-100">
+      <div className="w-full h-full max-w-[375px] max-h-[812px] bg-white border-1 mx-auto pt-[44px]">
         {children}
       </div>
     </div>

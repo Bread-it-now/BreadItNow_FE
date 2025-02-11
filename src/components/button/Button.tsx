9e8ab0d47df232b8f7b7cd633e251870c401cfb3
Sunghyun627 <kksscc4@gmail.com>
2025-02-11 12:04:01 +0900

    feat: button typorgraphy 설정 (#6)

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -31,6 +31,7 @@ const Button = ({
 
   const contentStyles = cn(
     `flex items-center justify-center w-full h-full gap-1.5 ${variant === "default" ? "text-black" : "text-white"}`,
+    "text-body-m",
   );
 
   return (
@@ -39,7 +40,7 @@ const Button = ({
       disabled={disabled}
       type={type ?? "button"}
     >
-      <div className={contentStyles}>{children}</div>
+      <div className={cn(contentStyles)}>{children}</div>
     </button>
   );
 };

21d759da0321a56dd22b506faaf93942ca2bee84
Sunghyun627 <kksscc4@gmail.com>
2025-02-12 15:03:32 +0900

    feat: Compount 패턴의 각 요소에 대한 interface 정의 (#6)

--- src/components/bottomsheet/Bottomsheet.tsx
+++ src/components/bottomsheet/Bottomsheet.tsx
@@ -1,43 +1,73 @@
 "use client";
 
 import { cn } from "@/utils/cn";
-import React from "react";
+import React, { HTMLAttributes, PropsWithChildren } from "react";
 import close from "@/assets/icons/close.svg";
 import Image from "next/image";
 
-interface BottomSheetHeaderProps {
+interface BottomSheetHeaderProps extends HTMLAttributes<HTMLDivElement> {
   title?: string;
   hasCloseBtn?: boolean;
+}
+
+interface BottomSheetContentProps
+  extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+  children: React.ReactNode;
+}
+
+interface BottomSheetButtonsProps
+  extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+  children: React.ReactNode;
+}
+
+interface BottomSheetContainerProps
+  extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+  children: React.ReactNode;
 }
 
 const BottomSheetHeader = ({
   title,
   hasCloseBtn = false,
+  className,
 }: BottomSheetHeaderProps) => {
   return (
-    <div className={cn("flex justify-between w-full h-6 px-5")}>
+    <div className={cn("flex justify-between w-full h-6 px-5", className)}>
       <p className={cn("text-lg font-semibold text-li")}>{title}</p>
       {hasCloseBtn && (
         <button className={cn("w-6 h-6")}>
-          <Image src={close} width={24} height={24} />
+          <Image src={close} width={24} height={24} alt="close" />
         </button>
       )}
     </div>
   );
 };
 
-const BottomSheetContent = ({ children }: { children: React.ReactNode }) => {
-  return <p className={cn("text-lg font-semibold text-li")}>{children}</p>;
+const BottomSheetContent = ({
+  children,
+  className,
+}: BottomSheetContentProps) => {
+  return (
+    <p className={cn("text-lg font-semibold text-li px-5", className)}>
+      {children}
+    </p>
+  );
 };
 
-const BottomSheetButtons = ({ children }: { children: React.ReactNode }) => {
-  return <div className={cn("flex gap-2 h-[92px]")}>{children}</div>;
+const BottomSheetButtons = ({
+  children,
+  className,
+}: BottomSheetButtonsProps) => {
+  return <div className={cn("flex gap-2 h-[92px]", className)}>{children}</div>;
 };
-const BottomSheetContainer = ({ children }: { children: React.ReactNode }) => {
+const BottomSheetContainer = ({
+  children,
+  className,
+}: BottomSheetContainerProps) => {
   return (
     <div
       className={cn(
-        "flex flex-col max-h-[752px] pt-[30px] bg-white rounded-t-xl",
+        "flex flex-col max-h-[752px] pt-[30px] bg-white rounded-t-xl gap-6 overflow-scroll",
+        className,
       )}
     >
       {children}

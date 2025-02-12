50b8db804817215c9e292eb99214472b89cee909
Sunghyun627 <kksscc4@gmail.com>
2025-02-12 20:20:15 +0900

    feat: bottomesheet dim 적용 (#6)

--- src/components/bottomsheet/Bottomsheet.tsx
+++ src/components/bottomsheet/Bottomsheet.tsx
@@ -14,36 +14,30 @@ interface BottomSheetContainerProps
 }
 
 const BottomSheetHeader = () => {
-  return <div className={cn("w-full h-[30px] bg-white rounded-t-xl")}></div>;
+  return <div className={cn("w-full h-[30px] bg-white rounded-t-3xl")}></div>;
 };
 
-const BottomSheetContent = ({
-  children,
-  className,
-}: BottomSheetContentProps) => {
+const BottomSheetContent = ({ children }: BottomSheetContentProps) => {
   return (
-    <p
-      className={cn(
-        "fixed top-[calc(100%-30px)] flex flex-col w-full bg-white gap-6 overflow-scroll",
-        className,
-      )}
-    >
-      {children}
-    </p>
+    <div className={cn("flex flex-col w-full bg-white gap-6")}>{children}</div>
   );
 };
 
 const BottomSheet = ({ children, className }: BottomSheetContainerProps) => {
   return (
-    <div
-      className={cn(
-        "absolute top-0 w-screen h-screen max-w-[375px] max-h-[812px] bg-black opacity-50",
-        className,
-      )}
-    >
-      <BottomSheetHeader />
-      <BottomSheetContent>{children}</BottomSheetContent>
-    </div>
+    <>
+      <div
+        className={cn(
+          "absolute top-0 w-screen h-screen max-w-[375px] max-h-[812px] bg-[rgba(0, 0, 0, 0.5)]",
+          className,
+        )}
+      >
+        <div className="relative top-[60px] w-full">
+          <BottomSheetHeader />
+          <BottomSheetContent>{children}</BottomSheetContent>
+        </div>
+      </div>
+    </>
   );
 };
 

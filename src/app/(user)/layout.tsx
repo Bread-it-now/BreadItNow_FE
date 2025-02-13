9ad1cef5063347865bc0d825c7efa83bf432bc03
Sunghyun627 <kksscc4@gmail.com>
2025-02-13 12:59:37 +0900

    feat: layout 초기화 (#6)

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -4,10 +4,8 @@ export default function UserLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex justify-center bg-gray-100">
-      <div className="w-full h-full max-w-[375px] max-h-[812px] bg-white border-1 mx-auto pt-[44px]">
-        {children}
-      </div>
+    <div className="">
+      <div className="w-full h-full">{children}</div>
     </div>
   );
 }

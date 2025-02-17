10e7b16cb0c62861e6ed01cd4b3ec8d84a0ef042
Dubabbi <thdms3160@naver.com>
2025-02-17 18:58:14 +0900

    feat: 하단 바에 현재 경로 감지 기능 추가

--- src/components/common/BottomNavbar.tsx
+++ src/components/common/BottomNavbar.tsx
@@ -1,20 +1,27 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import HomeIcon from "@/components/common/Icons/HomeIcon";
 import SearchIcon from "@/components/common/Icons/SearchIcon";
 import NotificationIcon from "@/components/common/Icons/NotificationIcon";
 import MyIcon from "@/components/common/Icons/MyIcon";
 
 export default function BottomNavbar() {
-  const [activeTab, setActiveTab] = useState("home");
   const router = useRouter();
+  const pathname = usePathname();
+
+  const activeTab = (() => {
+    if (pathname === "/") return "home";
+    if (pathname.startsWith("/search")) return "search";
+    if (pathname.startsWith("/notifications")) return "notifications";
+    if (pathname.startsWith("/my")) return "my";
+    return "";
+  })();
 
   const handleTabClick = (tab: string, path: string) => {
-    setActiveTab(tab);
     router.push(path);
   };
+
   return (
     <div className="w-full max-w-[375px] h-[58px] absolute bottom-0 left-0 bg-white rounded-t-2xl shadow-[0px_-1px_20px_0px_rgba(28,30,32,0.08)] flex justify-center items-center gap-8">
       <div
@@ -23,7 +30,9 @@ export default function BottomNavbar() {
       >
         <HomeIcon color={activeTab === "home" ? "#FF7651" : "#1C1E20"} />
         <span
-          className={`text-[11px] font-medium ${activeTab === "home" ? "text-[#FF7651]" : "text-[#1C1E20] opacity-60"}`}
+          className={`text-[11px] font-medium ${
+            activeTab === "home" ? "text-primary" : "text-gray900 opacity-60"
+          }`}
         >
           홈
         </span>
@@ -35,7 +44,9 @@ export default function BottomNavbar() {
       >
         <SearchIcon color={activeTab === "search" ? "#FF7651" : "#1C1E20"} />
         <span
-          className={`text-[11px] font-medium ${activeTab === "search" ? "text-[#FF7651]" : "text-[#1C1E20] opacity-60"}`}
+          className={`text-[11px] font-medium ${
+            activeTab === "search" ? "text-primary" : "text-gray900 opacity-60"
+          }`}
         >
           검색
         </span>
@@ -49,7 +60,11 @@ export default function BottomNavbar() {
           color={activeTab === "notifications" ? "#FF7651" : "#1C1E20"}
         />
         <span
-          className={`text-[11px] font-medium ${activeTab === "notifications" ? "text-[#FF7651]" : "text-[#1C1E20] opacity-60"}`}
+          className={`text-[11px] font-medium ${
+            activeTab === "notifications"
+              ? "text-primary"
+              : "text-gray900 opacity-60"
+          }`}
         >
           알림
         </span>
@@ -61,7 +76,9 @@ export default function BottomNavbar() {
       >
         <MyIcon color={activeTab === "my" ? "#FF7651" : "#1C1E20"} />
         <span
-          className={`text-[11px] font-medium ${activeTab === "my" ? "text-[#FF7651]" : "text-[#1C1E20] opacity-60"}`}
+          className={`text-[11px] font-medium ${
+            activeTab === "my" ? "text-primary" : "text-gray900 opacity-60"
+          }`}
         >
           마이
         </span>

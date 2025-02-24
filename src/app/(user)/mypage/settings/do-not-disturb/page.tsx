c2c7c4af6b008bf7dc572a9af658bfa1f9f43093
Sunghyun627 <kksscc4@gmail.com>
2025-02-24 09:27:06 +0900

    feat: 방해금지 switch 및 시간 설정 UI 구현 (#20)

--- src/app/(user)/mypage/settings/do-not-disturb/page.tsx
+++ src/app/(user)/mypage/settings/do-not-disturb/page.tsx
@@ -1,3 +1,39 @@
+'use client';
+
+import ToggleSwitch from '@/components/common/toggleswitch/ToggleSwitch';
+
 export default function Page() {
-  return <div>방해 금지 설정 페이지</div>;
+  return (
+    <>
+      <section>
+        <div className="flex items-center gap-5 px-5 py-[1.875rem] rounded-b-2xl w-full bg-white">
+          <div className={'flex flex-col items-start gap-[0.625rem] w-full'}>
+            <p className="text-title-content-m text-gray900">방해 금지 모드</p>
+            <p className="text-title-content-xs text-gray500 font-normal">
+              설정한 요일, 시간동안 모든 빵 알림이 오지 않습니다.
+            </p>
+          </div>
+          {<ToggleSwitch type="APP_NOTIFICATION" checked />}
+        </div>
+      </section>
+      <section className="flex flex-col items-start px-5 py-[1.875rem] gap-5 w-full bg-white rounded-2xl">
+        <p className="text-title-content-m text-gray900">시간 설정</p>
+        <div className="flex flex-col items-start gap-[0.625rem] w-full">
+          <div className="flex justify-between items-center w-full h-[33px]">
+            <span className="text-title-content-s font-medium">시작시간</span>
+            <button className="flex items-center justify-center px-[0.375rem] py-[0.75rem] rounded-lg w-[90px] h-full gap-2 bg-gray50">
+              <span className="text-body-m text-gray900 font-normal">오전 10:00</span>
+            </button>
+          </div>
+          <div className="flex justify-between items-center w-full h-[33px]">
+            <span className="text-title-content-s font-medium">종료시간</span>
+            <button className="flex items-center justify-center px-[0.375rem] py-[0.75rem] rounded-lg w-[90px] h-full gap-2 bg-gray50 ">
+              <span className="text-body-m text-gray900 font-normal">오전 10:00</span>
+            </button>
+          </div>
+        </div>
+      </section>
+      <section></section>
+    </>
+  );
 }

238595062516dc5fff5c8ce3dc5fe1340ba940a6
Sunghyun627 <kksscc4@gmail.com>
2025-02-26 08:01:35 +0900

    feat: 예약 취소상태 시, 하단에 예약 취소 버튼 비활성화 (#20)

--- src/app/(user)/mypage/reservations/[id]/page.tsx
+++ src/app/(user)/mypage/reservations/[id]/page.tsx
@@ -91,11 +91,13 @@ export default function Page() {
           <span className="text-right text-title-content-l text-primary w-full">{comma(totalPrice)}원</span>
         </div>
       </section>
-      <section className="p-5 w-full h-[92px] bg-white shadow-[0px-1px-20px-[rgba(28,30,32,0.08)] z-10">
-        <Button variant="default" fullWidth onClick={() => {}}>
-          예약취소
-        </Button>
-      </section>
+      {status !== 'CANCELED' && (
+        <section className="p-5 w-full h-[92px] bg-white shadow-[0px-1px-20px-[rgba(28,30,32,0.08)] z-10">
+          <Button variant="default" fullWidth onClick={() => {}}>
+            예약취소
+          </Button>
+        </section>
+      )}
     </>
   );
 }

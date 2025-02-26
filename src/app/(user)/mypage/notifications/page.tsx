f79bb15981564e37f294793962819574ccfc49a6
Sunghyun627 <kksscc4@gmail.com>
2025-02-26 09:53:44 +0900

    feat: 원하는 알림 타입을 선택하는 TabList 구현 (#20)

--- src/app/(user)/mypage/notifications/page.tsx
+++ src/app/(user)/mypage/notifications/page.tsx
@@ -1,3 +1,39 @@
+'use client';
+
+import RoundTab from '@/components/common/tabs/RoundTab';
+import { NotificationType } from '@/types/notification';
+import { useState } from 'react';
+import { notificatinoMockData } from '@/mocks/data/notification';
+import NotificationCard from '@/components/notifications/notificationCard';
+
 export default function Page() {
-  return <div>알림 내역 페이지</div>;
+  const [selectedCategory, setSelectedCategory] = useState<NotificationType | 'ALL'>('ALL');
+  const notifications = notificatinoMockData.filter((notification) => {
+    if (selectedCategory === 'ALL') return true;
+    return notification.type === selectedCategory;
+  });
+
+  return (
+    <>
+      <section className="flex items-center pl-5 py-[0.625rem] g-[0.375rem] h-[55px] overflow-y-auto bg-white border-b-[1px] border-b-gray100">
+        <RoundTab
+          categories={[
+            { key: 'ALL', label: '전체' },
+            { key: 'ALERT', label: '빵 알림' },
+            { key: 'RESERVATION', label: '예약' },
+          ]}
+          activeTab={selectedCategory}
+          onTabChange={setSelectedCategory}
+          className="px-0 py-0"
+        />
+      </section>
+      <section className="flex flex-col justify-center items-start gap-4 px-5 pt-6 pb-[50px] w-full">
+        <div className="flex flex-col items-start gap-[0.625rem] w-full">
+          {notifications.map((notification) => (
+            <NotificationCard key={notification.notificationId} {...notification} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }

601672bd1b72e3ce33c9497d3750142b9bcd841a
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2025-03-01 21:41:54 +0900

    design: menuCategory 디자인 수정

--- src/components/breadStoreInfo/MenuCategory.tsx
+++ src/components/breadStoreInfo/MenuCategory.tsx
@@ -1,7 +1,7 @@
-import RoundTab from "../common/tabs/RoundTab";
-import MenuList from "@/components/breadStoreInfo/MenuList";
-import { useState, memo } from "react";
-import { Product } from "@/types/product";
+import RoundTab from '../common/tabs/RoundTab';
+import MenuList from '@/components/breadStoreInfo/MenuList';
+import { useState, memo } from 'react';
+import { Product } from '@/types/product';
 interface MenuCategoryProps {
   key: string;
   label: string;
@@ -9,91 +9,78 @@ interface MenuCategoryProps {
 
 const menuCategories: MenuCategoryProps[] = [
   {
-    key: "1",
-    label: "빵류",
+    key: '1',
+    label: '빵류',
   },
   {
-    key: "2",
-    label: "기타",
+    key: '2',
+    label: '기타',
   },
 ];
 const breadList: Product[] = [
   {
-    productId: "1",
-    bakery_id: "1",
-    type: "BREAD",
-    name: "소금빵",
+    productId: '1',
+    bakery_id: '1',
+    type: 'BREAD',
+    name: '소금빵',
     price: 1000,
     stock: 10,
-    description: "소금빵 소개",
-    image: "https://placehold.co/600x400/png",
+    description: '소금빵 소개',
+    image: 'https://placehold.co/600x400/png',
     isActive: true,
   },
   {
-    productId: "2",
-    bakery_id: "1",
-    type: "BREAD",
-    name: "휘낭시에",
+    productId: '2',
+    bakery_id: '1',
+    type: 'BREAD',
+    name: '휘낭시에',
     price: 1000,
     stock: 10,
-    description: "휘낭시에 소개",
-    image: "https://placehold.co/600x400/png",
+    description: '휘낭시에 소개',
+    image: 'https://placehold.co/600x400/png',
     isActive: true,
   },
   {
-    productId: "3",
-    bakery_id: "1",
-    type: "BREAD",
-    name: "마들렌",
+    productId: '3',
+    bakery_id: '1',
+    type: 'BREAD',
+    name: '마들렌',
     price: 1000,
     stock: 10,
-    description: "마들렌 소개",
-    image: "https://placehold.co/600x400/png",
+    description: '마들렌 소개',
+    image: 'https://placehold.co/600x400/png',
     isActive: true,
   },
   {
-    productId: "4",
-    bakery_id: "1",
-    type: "BREAD",
-    name: "식빵",
+    productId: '4',
+    bakery_id: '1',
+    type: 'BREAD',
+    name: '식빵',
     price: 1000,
     stock: 10,
-    description: "식빵 소개",
-    image: "https://placehold.co/600x400/png",
+    description: '식빵 소개',
+    image: 'https://placehold.co/600x400/png',
     isActive: true,
   },
   {
-    productId: "5",
-    bakery_id: "1",
-    type: "BREAD",
-    name: "크루아상",
+    productId: '5',
+    bakery_id: '1',
+    type: 'BREAD',
+    name: '크루아상',
     price: 1000,
     stock: 10,
-    description: "크루아상 소개",
-    image: "https://placehold.co/600x400/png",
+    description: '크루아상 소개',
+    image: 'https://placehold.co/600x400/png',
     isActive: true,
   },
 ];
 const MemoizedMenuList = memo(
-  ({
-    category,
-    breadMenu,
-    otherMenu,
-  }: {
-    category: string;
-    breadMenu: Product[];
-    otherMenu: Product[];
-  }) => {
-    return (
-      <MenuList
-        menuList={category === "1" ? breadMenu : otherMenu}
-        title={category === "1" ? "빵류" : "기타"}
-      />
-    );
+  ({ category, breadMenu, otherMenu }: { category: string; breadMenu: Product[]; otherMenu: Product[] }) => {
+    return <MenuList menuList={category === '1' ? breadMenu : otherMenu} title={category === '1' ? '빵류' : '기타'} />;
   },
 );
 
-MemoizedMenuList.displayName = "MemoizedMenuList";
+MemoizedMenuList.displayName = 'MemoizedMenuList';
 
 function MenuCategory() {
   const [category, setCategory] = useState<string>(menuCategories[0].key);
@@ -105,18 +92,10 @@ function MenuCategory() {
   };
   return (
     <>
-      <div className="px-5">
-        <RoundTab
-          categories={menuCategories}
-          activeTab={category}
-          onTabChange={onTabChange}
-        />
+      <div>
+        <RoundTab categories={menuCategories} activeTab={category} onTabChange={onTabChange} />
       </div>
-      <MemoizedMenuList
-        category={category}
-        breadMenu={breadList}
-        otherMenu={[]}
-      />
+      <MemoizedMenuList category={category} breadMenu={breadList} otherMenu={[]} />
     </>
   );
 }

86047df6a6a59ef53e1207379b29be3f733ba7d6
Sunghyun627 <kksscc4@gmail.com>
2025-03-04 23:49:20 +0900

    feat: 프로필 정보 Section 구현 (#25)

--- src/app/(user)/mypage/page.tsx
+++ src/app/(user)/mypage/page.tsx
@@ -2,12 +2,19 @@
 
 import Image from 'next/image';
 import bell from '@/assets/icons/bell.svg';
+import profile from '@/assets/images/profile.png';
+import smallArrow from '@/assets/icons/arrow-left-sub.svg';
+import { useRouter } from 'next/navigation';
+import { ROUTES } from '@/constants/routes';
 
 const notificationCnt: number = 8;
+const name: string = '빵잇나우';
 
 export default function Page() {
+  const router = useRouter();
   return (
     <>
+      {/* 알림 컴포넌트  */}
       <button className="absolute right-5 top-[3.625rem]" onClick={() => {}}>
         <span
           className={'relative w-[1.5rem] h-[1.5rem] text-right text-body-m font-medium text-primary hover:opacity-70'}>
@@ -19,6 +26,18 @@ export default function Page() {
           )}
         </span>
       </button>
+      <section className="flex items-center px-5 pt-6 pb-[1.875rem] gap-4 w-full bg-white rounded-b-2xl">
+        <div className="w-[3.75rem] h-]3.75rem]">
+          <Image src={profile} width={60} height={60} alt="profile" className="border bg-gray50 rounded-full" />
+        </div>
+        <div className="flex flex-col items-start gap-1 w-full">
+          <p className="flex items-center gap-[2px] w-full h-[1.625rem] text-title-content-l">{name}</p>
+          <button className="flex items-center gap-[2px]" onClick={() => router.push(ROUTES.MYPAGE.CHECK_PASSWPRD)}>
+            <span className="text-title-content-xs font-normal text-gray500">내 정보 수정</span>
+            <Image src={smallArrow} width={12} height={12} alt="arrow" />
+          </button>
+        </div>
+      </section>
     </>
   );
 }
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -8,7 +8,7 @@ export const ROUTES = {
   MYPAGE: {
     HOME: '/mypage',
     CHECK_PASSWPRD: '/mypage/check-password',
-    EDIT_PROFILE: '/mypage/settings/profile',
+    PROFILE_SETTING: '/mypage/settings/profile',
     BREAD_NOTIFICATIONS_SETTING: '/mypage/settings/bread-notifications',
     APP_NOTIFICATIONS_SETTING: '/mypage/settings/notifications',
     DO_NOT_DISTURB: '/mypage/settings/do-not-disturb',
@@ -22,7 +22,7 @@ export const ROUTES = {
 export const MYPAGE_TITLE: Record<keyof typeof ROUTES.MYPAGE, string> = {
   HOME: '마이페이지',
   CHECK_PASSWPRD: '비밀번호 확인',
-  EDIT_PROFILE: '내 정보 수정',
+  PROFILE_SETTING: '내 정보 수정',
   BREAD_NOTIFICATIONS_SETTING: '빵 알림 설정',
   APP_NOTIFICATIONS_SETTING: '알림 설정',
   DO_NOT_DISTURB: '방해금지 모드',

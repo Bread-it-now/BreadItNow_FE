dee9693d5993904fb452ec8d9ae81cfa823f4df6
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2025-03-07 00:42:33 +0900

    refactor: ImageSlider 초기 인덱스 설정

--- src/components/common/slider/ImageSlider.tsx
+++ src/components/common/slider/ImageSlider.tsx
@@ -1,17 +1,12 @@
-"use client";
-import { useCallback } from "react";
-import { useImageSlider } from "@/hooks/useSlideEvent";
+'use client';
+import { useCallback } from 'react';
+import { useImageSlider } from '@/hooks/useSlideEvent';
 interface ImageSliderProps {
   images?: string[];
+  startIndex?: number;
 }
 
-function TagPagination({
-  currentPage,
-  lastPage,
-}: {
-  currentPage: number;
-  lastPage: number;
-}) {
+function TagPagination({ currentPage, lastPage }: { currentPage: number; lastPage: number }) {
   return (
     <div className="absolute bg-gray-900 backdrop-blur-[20px] bg-opacity-40 bottom-5 right-5 py-1 px-[14px] font-semibold rounded-2xl text-[11px]">
       {currentPage + 1}
@@ -22,11 +17,8 @@ function TagPagination({
 }
 
 const ImageSlider = ({
-  images = [
-    "https://placehold.co/300x400",
-    "https://placehold.co/600x400",
-    "https://placehold.co/800x1000",
-  ],
+  images = ['https://placehold.co/300x400', 'https://placehold.co/600x400', 'https://placehold.co/800x1000'],
+  startIndex = 0,
 }: ImageSliderProps) => {
   const {
     currentIndex,
@@ -39,15 +31,14 @@ const ImageSlider = ({
     handleDragEnd,
     handleMouseDown,
     handleMouseMove,
-  } = useImageSlider({ images });
+  } = useImageSlider({ images, startIndex });
 
   // 드래그시 트랜스폼 스타일 계산
   const getTransformStyle = useCallback<() => string>(() => {
     if (isDragging) {
       const dragDistance = touchEnd - touchStart;
       const baseTransform = -currentIndex * 100;
-      const dragPercentage =
-        (dragDistance / (sliderRef.current?.offsetWidth ?? 1)) * 100;
+      const dragPercentage = (dragDistance / (sliderRef.current?.offsetWidth ?? 1)) * 100;
       return `translateX(${baseTransform + dragPercentage}%)`;
     }
     return `translateX(-${currentIndex * 100}%)`;
@@ -67,14 +58,10 @@ const ImageSlider = ({
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleDragEnd}
-        onMouseLeave={handleDragEnd}
-      >
+        onMouseLeave={handleDragEnd}>
         {images.map((image, index) => (
           <div key={`slider-${index}`} className="min-w-full h-full">
-            <div
-              className="w-full h-full bg-center bg-cover"
-              style={{ backgroundImage: `url(${image})` }}
-            />
+            <div className="w-full h-full bg-center bg-cover" style={{ backgroundImage: `url(${image})` }} />
           </div>
         ))}
       </div>

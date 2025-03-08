164d871021d8f6b64eadfbef980beae19f198725
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2025-03-08 23:14:56 +0900

    design: height 값 수정

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,7 +1,7 @@
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="justify-center bg-gray-100">
-      <div className="relative min-h-screen overflow-y-auto bg-gray-50 w-screen max-w-screen border-1">
+      <div className="relative max-h-screen h-screen overflow-y-auto bg-gray-50 w-screen max-w-screen border-1">
         <div className="bg-gray-50 ">{children}</div>
       </div>
       <div id="bottomsheet-root" />

2ec970eb9c79b96c77a6cfb0c366f9fe8952524c
Sunghyun627 <kksscc4@gmail.com>
2025-03-17 23:46:46 +0900

    feat: 빵집 관리 페이지에서 빵집 정보 api 호출 (#37)

--- src/app/owner/page.tsx
+++ src/app/owner/page.tsx
@@ -4,19 +4,42 @@ import ProductStockCard, { ProductStockCardProps } from '@/components/productsto
 import { mockProducts } from '@/mocks/data/product';
 import Stack from '@/components/common/stack/Stack';
 import Button from '@/components/button/Button';
+import { getBakeryInfo } from '@/lib/api/bakery';
+import { useQuery } from '@tanstack/react-query';
+import { Bakery } from '@/types/bakery';
+import { Fragment } from 'react';
+
+const bakeryId: number = 1;
 
 /** 로그인 시 가져온 bakeryId를 통해 빵집 정보 호출 */
-const name = '소금 한 꼬집';
+
+const OperatingSection = ({ bakeryId }: { bakeryId: number }) => {
+  const { data: bakery } = useQuery({
+    queryKey: ['bakery'],
+    queryFn: () => getBakeryInfo(bakeryId),
+    select: (data: { data: Bakery }) => data?.data,
+  });
+
+  return (
+    <>
+      {bakery && (
+        <Fragment>
+          <section className="w-full bg-white rounded-b-[0.625rem]">
+            <OperatingStatusCard name={bakery.name} operatingStatus="TEMPORARY_CLOSED" type="GENERAL" />
+          </section>
+          <section className="w-full bg-white rounded-[0.625rem]">
+            <OperatingStatusCard operatingStatus="TEMPORARY_CLOSED" type="TEMPORARY" />
+          </section>
+        </Fragment>
+      )}
+    </>
+  );
+};
 
 export default function Page() {
   return (
     <>
-      <section className="w-full bg-white rounded-b-[0.625rem]">
-        <OperatingStatusCard name={name} operatingStatus="TEMPORARY_CLOSED" type="GENERAL" />
-      </section>
-      <section className="w-full bg-white rounded-[0.625rem]">
-        <OperatingStatusCard operatingStatus="TEMPORARY_CLOSED" type="TEMPORARY" />
-      </section>
+      <OperatingSection bakeryId={bakeryId} />
       <section className="flex flex-col items-start px-5 py-[1.875rem] gap-6 w-full bg-white rounded-2xl">
         <div className="flex justify-between items-center gap-[0.625rem] w-full">
           <p className="text-title-content-m text-gray900">예약 상품</p>

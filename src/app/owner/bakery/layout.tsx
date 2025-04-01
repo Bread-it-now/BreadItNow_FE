2995ec4fa30093001a451795ddb0775f01f3112e
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2025-04-01 22:23:50 +0900

    refactor: layout에 bottom-sheet 제거

--- src/app/owner/bakery/layout.tsx
+++ src/app/owner/bakery/layout.tsx
@@ -2,7 +2,6 @@ export default function OwnerLayout({ children }: { children: React.ReactNode })
   return (
     <>
       <div className="bg-gray-50">{children}</div>
-      <div id="bottomsheet-root" />
     </>
   );
 }

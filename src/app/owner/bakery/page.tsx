231c0b590b6213a9b507eb8d8eef13a9881d5e49
Sunghyun627 <kksscc4@gmail.com>
2025-04-03 15:23:16 +0900

    refactor: 순서 조정 바텀 시트 분리 (#43)

--- src/app/owner/bakery/page.tsx
+++ src/app/owner/bakery/page.tsx
@@ -2,9 +2,9 @@
 import Button from '@/components/button/Button';
 import HotBreadTab from '@/components/common/tabs/HotBreadTab';
 import { useBakeryProducts } from '@/lib/api/bakery';
-import { SetStateAction, useEffect, useState } from 'react';
+import { useState } from 'react';
 import Stack from '@/components/common/stack/Stack';
-import { BakeryProducts, Product, ProductOrder } from '@/types/bakery';
+import { Product } from '@/types/bakery';
 import ProductCard from '@/components/productcard/ProductCard';
 import useEditProductBottomSheet from '@/hooks/useEditProductBottomSheet';
 import BottomSheet from '@/components/bottomsheet/Bottomsheet';
@@ -17,10 +17,7 @@ import { useRouter } from 'next/navigation';
 import { ROUTES } from '@/constants/routes';
 import useDeleteProductsBottomSheet from '@/hooks/useDeleteProductsBottomSheet';
 import useReorderProductsBottomSheet from '@/hooks/useReorderProductsBottomSheet';
-import { DndContext, DragEndEvent } from '@dnd-kit/core';
-import { SortableContext, arrayMove, verticalListSortingStrategy } from '@dnd-kit/sortable';
-import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
+import ReorderProductsBottomSheet from '@/components/bottomsheet/ ReorderProductsBottomSheet/ ReorderProductsBottomSheet';
 
 const HEADER_TABS = [
   { key: 'bakeryInfo', label: '빵집정보' },
@@ -212,6 +209,7 @@ export default function Page() {
               close={closeReorderProductsBottomSheet}
               isOpen={isReorderProductsBottomSheetOpen}
               reorderProducts={reorderProducts}
+              bakeryId={bakeryId}
             />
           )}
         </div>
@@ -219,135 +217,3 @@ export default function Page() {
     </div>
   );
 }
-
-const SortableProduct = ({ product }: { product: Product }) => {
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: product.productId });
-
-  return (
-    <div
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-      className="w-full"
-      style={{
-        transform: CSS.Transform.toString(transform),
-        transition,
-      }}>
-      <ProductCard
-        key={`${product.productId}-${product.name}`}
-        {...product}
-        isDraggable
-        className="hover:cursor-pointer"
-      />
-    </div>
-  );
-};
-
-const SortedProductsList = ({
-  title,
-  initProducts,
-  startIdx,
-  handleReorder,
-}: {
-  title: string;
-  initProducts: Product[];
-  startIdx: number;
-  handleReorder: React.Dispatch<SetStateAction<ProductOrder[]>>;
-}) => {
-  const [products, setProducts] = useState<Product[]>([...initProducts]);
-
-  useEffect(() => {
-    handleReorderedProducts();
-  }, [products]);
-
-  const handleReorderedProducts = () => {
-    const _reorderedProducts: { productId: number; order: number }[] = products
-      .map((product: Product, idx) => {
-        return { productId: product.productId, order: idx + startIdx };
-      })
-      .filter(({ productId }, idx) => productId !== initProducts[idx].productId);
-    handleReorder([..._reorderedProducts]);
-  };
-
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-
-    if (active.id === over?.id) return;
-
-    const prevIdx = products.findIndex((product: Product) => product.productId === active.id);
-    const newIdx = products.findIndex((product: Product) => product.productId === over?.id);
-
-    if (prevIdx === -1 || newIdx === -1) return;
-
-    setProducts(arrayMove(products, prevIdx, newIdx));
-    handleReorderedProducts();
-  };
-  return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {/* 빵류 제품 */}
-      <div className="flex flex-col gap-6 w-full rounded-2xl bg-white">
-        <p className="text-title-content-l font-semibold">{title}</p>
-        <div className="flex flex-col justify-center items-start gap-4 w-full h-full min-[133px]">
-          <SortableContext items={products.map((product) => product.productId)} strategy={verticalListSortingStrategy}>
-            <Stack divider={<div className="w-full h-[1px] bg-gray-100"></div>}>
-              {products.map((product: Product) => (
-                <SortableProduct key={`${product.productId}-${product.name}`} product={product} />
-              ))}
-            </Stack>
-          </SortableContext>
-        </div>
-      </div>
-    </DndContext>
-  );
-};
-
-const ReorderProductsBottomSheet = ({
-  productsInfo,
-  close,
-  isOpen,
-  reorderProducts,
-}: {
-  productsInfo: BakeryProducts;
-  close: () => void;
-  isOpen: boolean;
-  reorderProducts: (productOrders: ProductOrder[]) => void;
-}) => {
-  const [reorderedBreadProducts, setReorderedBreadProducts] = useState<ProductOrder[]>([]);
-  const [reorderedOtherProducts, setReorderedOtherProducts] = useState<ProductOrder[]>([]);
-  const { breadProducts, otherProducts } = productsInfo;
-  const queryClient = useQueryClient();
-
-  return (
-    <BottomSheet
-      isOpen={isOpen}
-      onClose={close}
-      fullHeight
-      title="메뉴 순서 변경"
-      confirmText="적용"
-      onConfirm={() => {
-        reorderProducts([...reorderedBreadProducts, ...reorderedOtherProducts]);
-        close();
-        queryClient.invalidateQueries({
-          queryKey: [...BAKERY_QUERY_KEY.BAKERY_PRODUCTS(bakeryId)],
-        });
-      }}
-      confirmDisabled={reorderedBreadProducts.length === 0 && reorderedOtherProducts.length === 0}>
-      <div className="flex flex-col itmes-center gap-[30px] w-full bg-white">
-        <>
-          <SortedProductsList
-            initProducts={breadProducts}
-            title="빵류"
-            startIdx={0}
-            handleReorder={setReorderedBreadProducts}
-          />
-          <SortedProductsList
-            initProducts={otherProducts}
-            title="기타"
-            startIdx={breadProducts.length}
-            handleReorder={setReorderedOtherProducts}
-          />
-        </>
-      </div>
-    </BottomSheet>
-  );
-};

7cc700f4209889354eb17816597d4717eb9a784c
Sunghyun627 <kksscc4@gmail.com>
2025-04-05 16:27:40 +0900

    feat: 컴포넌트 우측 상단에 필수 여부를 표시하는 원 추가 (#46)

--- src/components/common/labelform/LabelForm.tsx
+++ src/components/common/labelform/LabelForm.tsx
@@ -14,6 +14,9 @@ export interface LabelFormProps<T extends FieldValues = FieldValues> {
 
   /** 비활성화 여부 */
   disabled?: boolean;
+
+  /** 핖수 값인지에 대한 boolea */
+  isRequired?: boolean;
 }
 
 export const LabelForm = <T extends FieldValues = FieldValues>({
@@ -21,10 +24,16 @@ export const LabelForm = <T extends FieldValues = FieldValues>({
   children,
   name,
   errors,
+  isRequired,
 }: LabelFormProps<T>) => {
   return (
-    <div className="flex flex-col gap-2">
-      {label && <p>{label}</p>}
+    <div className="relative flex flex-col gap-2">
+      {label && (
+        <div className="flex gap-[2px]">
+          <span className="text-title-content-xs">{label}</span>
+          {isRequired && <span className="absolute top-0 right-[-6px] w-[3px] h-[3px] rounded-full bg-error" />}
+        </div>
+      )}
       {children}
       {errors?.[name]?.message && (
         <span className="text-title-content-2xs text-error font-medium">

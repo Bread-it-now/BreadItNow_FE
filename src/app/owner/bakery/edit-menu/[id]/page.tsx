0b0c88cc19fc3ff3fe15f05ebd0e8a120bdb6a08
Sunghyun627 <kksscc4@gmail.com>
2025-04-06 07:17:50 +0900

    feat: 메뉴 타입을 설정 LabelForm 구현 (#46)

--- src/app/owner/bakery/edit-menu/[id]/page.tsx
+++ src/app/owner/bakery/edit-menu/[id]/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { useBakeryProduct } from '@/lib/api/bakery';
+import { editProduct, useBakeryProduct } from '@/lib/api/bakery';
 import { ProductForm } from '@/types/bakery';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { useParams, useRouter } from 'next/navigation';
 import Button from '@/components/button/Button';
 import { ROUTES } from '@/constants/routes';
 import { LabelForm } from '@/components/common/labelform/LabelForm';
+import SelectedItem from '@/components/selecteditem/SelectItem';
 
 const bakeryId = 1;
 
@@ -16,31 +17,61 @@ export interface LayoutProps {
   productId?: number;
   initValue: ProductForm | null;
 }
-
-const Layout = ({ initValue }: LayoutProps) => {
+export const ProductFormLayout = ({ initValue, mutate }: LayoutProps) => {
   const router = useRouter();
-  const {} = useForm<ProductForm>({});
-  const { handleSubmit } = useForm<ProductForm>({
+
+  const {
+    handleSubmit,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm<ProductForm>({
     defaultValues: initValue || {},
   });
+  const data = watch();
 
   return (
     <div className="flex flex-col items-start w-full h-full bg-gray50">
       <form
         className="flex flex-col items-start gap-[30px] px-5 pt-6 pb-[30px] w-full bg-white"
-        onSubmit={handleSubmit(() => {})}>
-        <LabelForm name="productType" label="메뉴 타입" isRequired>
-          <div>12</div>
+        onSubmit={handleSubmit(() => {
+          mutate({ ...data, productType: data.productType });
+          router.push(ROUTES.OWNER.BAKERY.BAKERY_HOME);
+        })}>
+        <LabelForm name="productType" label="메뉴 타입" isRequired errors={errors} className="w-full">
+          <div className="flex flex-col items-start gap-1 w-full">
+            <div className="flex w-full gap-2">
+              <Controller
+                name="productType"
+                control={control}
+                render={({ field }) => (
+                  <div className="flex w-full gap-2">
+                    <SelectedItem
+                      label="빵류"
+                      checked={field.value === 'BREAD'}
+                      onClick={() => field.onChange('BREAD')}
+                    />
+                    <SelectedItem
+                      label="기타"
+                      checked={field.value === 'OTHER'}
+                      onClick={() => field.onChange('OTHER')}
+                    />
+                  </div>
+                )}
+                rules={{ required: '메뉴 타입을 선택해주세요.' }}
+              />
+            </div>
+            <p className="text-title-content-2xs text-gray500">
+              *기타에는 크림치즈, 생크림등 빵 관련 부가적인 메뉴를 등록합니다.
+            </p>
+          </div>
         </LabelForm>
+
         <div className="absolute bottom-0 left-0 flex p-5 gap-2 w-full shadow-[0px_-1px_20px_0px_rgba(28,30,32,0.08)] bg-white">
           <Button variant="default" fullWidth onClick={() => router.push(ROUTES.OWNER.BAKERY.BAKERY_HOME)}>
             취소
           </Button>
-          <Button
-            variant="primary"
-            type="submit"
-            fullWidth
-            onClick={() => router.push(ROUTES.OWNER.BAKERY.BAKERY_HOME)}>
+          <Button variant="primary" type="submit" fullWidth onClick={() => {}}>
             저장
           </Button>
         </div>
@@ -64,5 +95,14 @@ export default function Page() {
       }
     : null;
 
-  return product && <Layout type={'CREATE'} mutate={() => {}} initValue={initValue} productId={productId} />;
+  return (
+    product && (
+      <ProductFormLayout
+        type={'EDIT'}
+        mutate={(productForm: ProductForm) => editProduct(bakeryId, productId, productForm)}
+        initValue={initValue}
+        productId={productId}
+      />
+    )
+  );
 }

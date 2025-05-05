ce6caa48f2ffa09a48854affe58a51ad5a11f14d
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2025-05-05 14:39:38 +0900

    fix: token 수정

--- src/app/social/callback/page.tsx
+++ src/app/social/callback/page.tsx
@@ -3,28 +3,35 @@
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { ROUTES } from '@/constants/routes';
+// import { ROUTES } from '@/constants/routes';
 export default function SocialCallbackPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const handleTokenRefresh = async () => {
     try {
-      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth-api/api/v1/token/refresh`, {
+      const result = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth-api/api/v1/token/refresh`, {
         method: 'POST',
         credentials: 'include',
         headers: { 'Content-Type': 'application/json' },
       });
+      const accessToken = result.headers.get('Authorization');
+      if (accessToken) {
+        localStorage.setItem('accessToken', accessToken);
+      }
+      return result;
     } catch (error) {
       throw error;
     }
   };
 
   useEffect(() => {
-    if (searchParams.get('isNewUser') === 'true') {
-      router.push(ROUTES.AUTH.LOGIN + '?isNewUser=true');
-    } else {
-      handleTokenRefresh().then(() => router.push('/'));
-    }
+    handleTokenRefresh();
+    // if (searchParams.get('isNewUser') === 'true') {
+    //   router.push(ROUTES.AUTH.LOGIN + '?isNewUser=true');
+    // } else {
+    //   handleTokenRefresh().then(() => router.push('/'));
+    // }
+    // handleTokenRefresh().then(() => router.push('/'));
   }, [searchParams, router]);
   return <div></div>;
 }
--- src/lib/customFetch.ts
+++ src/lib/customFetch.ts
@@ -22,6 +22,7 @@ export const customFetch = async (url: string, options: RequestInit): Promise<Re
 
   const handleTokenRefresh = async () => {
     try {
+      localStorage.removeItem('accessToken');
       const response = await fetch(`${baseUrl}/auth-api/api/v1/token/refresh`, {
         method: 'POST',
         credentials: 'include',

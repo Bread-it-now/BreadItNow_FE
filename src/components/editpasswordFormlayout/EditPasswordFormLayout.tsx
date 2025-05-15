d0651599a43dc20153daedfde19a7420c691355d
Sunghyun627 <kksscc4@gmail.com>
2025-05-15 11:01:07 +0900

    feat: 비밀번호 validate 로직 추가 (#66)

--- src/components/editpasswordFormlayout/EditPasswordFormLayout.tsx
+++ src/components/editpasswordFormlayout/EditPasswordFormLayout.tsx
@@ -32,12 +32,24 @@ export const EditPasswordFormLayout = ({ mutate }: EditPasswordFormLayoutProps) 
         className="flex flex-col items-start gap-2 px-5 pt-6 pb-[102px] w-full bg-white"
         onSubmit={handleSubmit(() => {
           mutate(data);
-          router.push(ROUTES.OWNER.BAKERY.BAKERY_HOME);
+          router.push(ROUTES.OWNER.HOME);
         })}>
         <LabelForm name="newPassword" label="비밀 번호" errors={errors} className="w-full">
           <Controller
             control={control}
-            rules={{ required: '' }}
+            rules={{
+              required: '비밀번호를 입력해주세요.',
+              validate: (value) => {
+                const hasLetter = /[A-Za-z]/.test(value);
+                const hasNumber = /[0-9]/.test(value);
+                const hasSpecial = /[^A-Za-z0-9]/.test(value);
+                const isLongEnough = value.length >= 8;
+
+                if (!isLongEnough) return '8자 이상 입력해주세요.';
+                if (!hasLetter || !hasNumber || !hasSpecial) return '영문, 숫자, 특수기호를 모두 포함해야 합니다.';
+                return true;
+              },
+            }}
             {...register('newPassword')}
             render={({ field }) => {
               return (
@@ -56,7 +68,15 @@ export const EditPasswordFormLayout = ({ mutate }: EditPasswordFormLayoutProps) 
         <LabelForm name="confirmNewPassword" errors={errors} className="w-full">
           <Controller
             control={control}
-            rules={{ required: '' }}
+            rules={{
+              required: '비밀번호를 한 번 더 입력해주세요.',
+              validate: (value) => {
+                if (value !== watch('newPassword')) {
+                  return '비밀번호가 일치하지 않습니다.';
+                }
+                return true;
+              },
+            }}
             {...register('confirmNewPassword')}
             render={({ field }) => {
               return (
@@ -74,7 +94,7 @@ export const EditPasswordFormLayout = ({ mutate }: EditPasswordFormLayoutProps) 
         </LabelForm>
 
         <div className="absolute bottom-0 left-0 flex p-5 gap-2 w-full shadow-[0px_-1px_20px_0px_rgba(28,30,32,0.08)] bg-white">
-          <Button variant="default" fullWidth onClick={() => router.push(ROUTES.OWNER.BAKERY.BAKERY_HOME)}>
+          <Button variant="default" fullWidth onClick={() => router.push(ROUTES.OWNER.BAKERY.SETTING_HOME)}>
             취소
           </Button>
           <Button variant="primary" type="submit" disabled={!isDirty} fullWidth onClick={() => {}}>

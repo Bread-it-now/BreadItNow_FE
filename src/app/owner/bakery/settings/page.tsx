4306e792e009cbbfeff0d61350dc72dd195e71c9
Sunghyun627 <kksscc4@gmail.com>
2025-05-15 09:09:08 +0900

    feat: owner 설정 페이지 레이아웃 구현 (#66)

--- src/app/owner/bakery/settings/page.tsx
+++ src/app/owner/bakery/settings/page.tsx
@@ -1,7 +1,31 @@
+'use client';
+import Stack from '@/components/common/stack/Stack';
+import NavLink from '@/components/navlink/NavLink';
+import inquiry from '@/assets/icons/inquiry.svg';
+import { ROUTES } from '@/constants/routes';
+import lock from '@/assets/icons/lock.svg';
+import store from '@/assets/icons/store.svg';
+
 export default function Page() {
   return (
-    <div>
-      <h1>설정 페이지</h1>
+    <div className="min-h-screen flex flex-col gap-4 bg-gray50">
+      <section className="flex flex-col items-start px-5 py-[1.875rem] gap-5 w-full rounded-2xl bg-white">
+        <Stack divider={<div className="w-full h-[1px] bg-gray100"></div>}>
+          <NavLink title={'비밀번호 수정'} icon={lock} targetUrl={ROUTES.OWNER.BAKERY.EDIT_PROFILE} />
+          <NavLink title={'빵집 삭제'} icon={store} targetUrl={''} showBtn={false} />
+        </Stack>
+      </section>
+      <section className="flex flex-col items-start px-5 py-[1.875rem] gap-5 w-full rounded-2xl bg-white">
+        <NavLink title={'빵잇나우팀에게 문의하기'} icon={inquiry} targetUrl={ROUTES.OWNER.HOME} />
+      </section>
+      <section className="flex w-full text-title-content-xs px-5">
+        <button className="w-full text-gray500" onClick={() => {}}>
+          로그아웃
+        </button>
+        <button className="w-full text-primary" onClick={() => {}}>
+          회원탈퇴
+        </button>
+      </section>
     </div>
   );
 }
